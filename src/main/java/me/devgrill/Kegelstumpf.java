449f3d9d195ff35c79e24c9d02abbf00ba316d36
Grill <cyril.kurmann@gmail.com>
2022-11-25 09:06:41 +0100

    finished Kreis class

--- src/main/java/me/devgrill/Kegelstumpf.java
+++ src/main/java/me/devgrill/Kegelstumpf.java
@@ -1,9 +1,38 @@
 package me.devgrill;
 
 public class Kegelstumpf {
-    private Kreis m_Deckkreis;
-    private
-    public Kegelstumpf(){
+    private Kreis m_Deckkreis = null;
+    private Kreis m_Grundkreis = null;
+    private double m_Hoehe;
+    public Kegelstumpf(Kreis m_Deckkreis, Kreis m_Grundkreis, double m_Hoehe){
+        this.m_Deckkreis = m_Deckkreis;
+        this.m_Grundkreis = m_Grundkreis;
+        this.m_Hoehe = m_Hoehe;
+    }
 
+    public Kegelstumpf(double m_Deckkreis, double m_Grundkreis, double m_Hoehe){
+        this.m_Deckkreis = new Kreis(m_Deckkreis);
+        this.m_Grundkreis = new Kreis(m_Grundkreis);
+        this.m_Hoehe = m_Hoehe;
+    }
+
+    public double getMantellinie(){
+
+    }
+
+    public double getMantelflaeche(){
+
+    }
+
+    public double getOberflaeche(){
+
+    }
+
+    public double getVolumen(){
+
+    }
+
+    public void setHoehe(double m_Hoehe){
+        this.m_Hoehe = m_Hoehe;
     }
 }
--- src/main/java/me/devgrill/Kreis.java
+++ src/main/java/me/devgrill/Kreis.java
@@ -1,4 +1,34 @@
 package me.devgrill;
 
 public class Kreis {
+    private double m_Durchmesser;
+
+    public Kreis(double m_Durchmesser){
+        this.m_Durchmesser = m_Durchmesser;
+    }
+
+    public Kreis(){
+
+    }
+
+    public double getDurchmesser(){
+        return this.m_Durchmesser;
+    }
+
+    public double getFlaeche(){
+        double m_Flaeche = this.m_Durchmesser * Math.pow(getRadius(), 2) * Math.PI;
+        return m_Flaeche;
+    }
+
+    public double getRadius(){
+        return (this.m_Durchmesser / 2);
+    }
+
+    public void setDurchmesser(double m_Durchmesser){
+        this.m_Durchmesser = m_Durchmesser;
+    }
+
+    public void setRadius(double m_Radius){
+        this.m_Durchmesser = (m_Radius * 2);
+    }
 }
